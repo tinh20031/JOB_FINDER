7a72a3329411067a8f79362c86b4c45198a8cae2
thanhtungt <tungccvv111@gmail.com>
2025-05-24 01:18:02 +0700

    return update fullcloundinary

--- JOB_FINDER_API/obj/Debug/net8.0/JOB_FINDER_API.AssemblyInfo.cs
+++ JOB_FINDER_API/obj/Debug/net8.0/JOB_FINDER_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JOB_FINDER_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc0f924d7c283c4fc2baf7281d5b1f122d0504b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+993c38b3e8cfc81a67f24e672bd33fe67908e688")]
 [assembly: System.Reflection.AssemblyProductAttribute("JOB_FINDER_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JOB_FINDER_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

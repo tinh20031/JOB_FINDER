4969f689e29e8a25ce21aeaa4c63df372505ce22
tinh20031 <tinhtranquocx@gmail.com>
2025-06-29 20:28:34 +0700

    update

--- JOB_FINDER_API/obj/Debug/net8.0/JOB_FINDER_API.AssemblyInfo.cs
+++ JOB_FINDER_API/obj/Debug/net8.0/JOB_FINDER_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JOB_FINDER_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb6ee7d98f0f6a46ac03774a07c38d9d6c40f6c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8226ad6bb1d60568f266176fcba8041e61d209e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("JOB_FINDER_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JOB_FINDER_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

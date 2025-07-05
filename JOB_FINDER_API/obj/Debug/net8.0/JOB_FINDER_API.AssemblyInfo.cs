e71c74c67f26e73e33ab72c721542b6faa0a659b
tinh20031 <tinhtranquocx@gmail.com>
2025-07-05 21:06:29 +0700

    merge code dinh

--- JOB_FINDER_API/obj/Debug/net8.0/JOB_FINDER_API.AssemblyInfo.cs
+++ JOB_FINDER_API/obj/Debug/net8.0/JOB_FINDER_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JOB_FINDER_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdc7d5f4a4521f97a876975aa7768cc2691e237d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c0742845253cc0a2f8f6bcf6610b15a6197d22d")]
 [assembly: System.Reflection.AssemblyProductAttribute("JOB_FINDER_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JOB_FINDER_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
